f807539413de6da043bc1de13d6e7b2937fc3e9c
cyw <869647291@qq.com>
2018-05-25 14:40:14 +0800

    Initial commit

---
+++ src/main/java/com/chen/producer/ProducerService.java
@@ -0,0 +1,6 @@
+package com.chen.producer;
+
+
+public interface ProducerService {
+    void sendMessage(String message);
+}
---
+++ src/main/java/com/chen/producer/ProducerServiceImpl.java
@@ -0,0 +1,27 @@
+package com.chen.producer;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.jms.core.MessageCreator;
+
+import javax.annotation.Resource;
+import javax.jms.*;
+
+
+public class ProducerServiceImpl implements ProducerService {
+    @Autowired
+    private JmsTemplate jmsTemplate;
+    @Resource(name="queueDestination")
+    Destination destination;
+
+    public void sendMessage(final String message) {
+        jmsTemplate.send(destination, new MessageCreator() {
+            public Message createMessage(Session session) throws JMSException {
+                TextMessage textMessage =  session.createTextMessage(message);
+                return textMessage;
+            }
+        });
+        System.out.println("发送消息:"+message);
+    }
+}
